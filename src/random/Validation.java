4b80bc7c509b8a4243fd04e5e9fb93379eee1a0a
komodochristopher <league@WTS13.attlocal.net>
2019-03-31 11:30:17 -0700

    On Validation
    :poop: JK :smile:

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -17,7 +17,14 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		for(int i=0;i<10;i++) {
+		if(randomNumber == 0) {JOptionPane.showMessageDialog(null, "you're foot fungus is almost nonexistent");}
+		else if(randomNumber == 1) {JOptionPane.showMessageDialog(null, "You would survive the zombie apocalypse because you're as ugly as an ugly person");}
+		else if(randomNumber == 2) {JOptionPane.showMessageDialog(null, "Are you always so stupid or is today a special occasion?");}
+		else if(randomNumber == 3) {JOptionPane.showMessageDialog(null, "As an outsider, what do you think of the human race?");}
+		else if(randomNumber == 4) {JOptionPane.showMessageDialog(null, "Hi, I’m a human being. What are you?");}
+		else if(randomNumber == 5) {JOptionPane.showMessageDialog(null, "Don’t think, it might sprain your brain.");}
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
